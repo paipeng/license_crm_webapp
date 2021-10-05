d169b133456b0ef09fb751bd946cb73554669a72
Pai Peng <p.peng@s2icode.com>
2021-10-05 22:27:43 +0800

    update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LicenseComponent } from './license/license.component';
 import { LicensesComponent } from './licenses/licenses.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'first-component', component: LoginComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'login/:tokenexpired', component: LoginComponent },
   {
     path: '',
     component: HomeComponent,
     children: [
       { path: 'licenses', component: LicensesComponent },
+      { path: 'licenses/:id', component: LicenseComponent },
       //{ path: '**', redirectTo: '/', pathMatch: 'full' },
       //{ path: '**', component: LoginComponent }
     ]
