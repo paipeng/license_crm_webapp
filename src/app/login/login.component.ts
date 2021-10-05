843bbd1af9f61fc17a023a6951ee046582318317
Pai Peng <p.peng@s2icode.com>
2021-10-05 15:17:47 +0800

    change to scss and set html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  passwordFlag: boolean;
+  saveUsername: boolean;
+  passwordImage: string;
+  codeImage: string;
+  constructor() {
 
-  constructor() { }
+    this.passwordFlag = true;
+    this.saveUsername = false;
+    this.passwordImage = '../erp/assets/images/password_hide.png';
+    this.codeImage = '';
+  }
 
   ngOnInit(): void {
   }
 
+  goWebsite() {
+
+  }
+
+  changePasswordFlag() {
+
+  }
+
+
+  keypress(event: any) {
+    if (event.keyCode === 13) {
+      this.login();
+    }
+  }
+
+  login() {
+  }
+
+  getCode() {
+
+  }
+
+  checkChanged() {
+    if (this.saveUsername) {
+      // this.cookie.set('lotteryUserName', this.formModel.value.username);
+    } else {
+      // this.cookie.delete('lotteryUserName');
+    }
+  }
 }
