29a25ba6bf8adb867ee0b63a98ab919d3790af96
Pai Peng <p.peng@s2icode.com>
2021-10-06 16:31:22 +0800

    load file and call verify

--- src/app/verify/verify.component.ts
+++ src/app/verify/verify.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { License } from '../models/license.model';
 import { LicenseBase64 } from '../models/licensebase64.model';
 import { DialogService } from '../service/dialog.service';
 import { LicenseService } from '../service/license.service';
@@ -12,6 +13,10 @@ import { LicenseService } from '../service/license.service';
 export class VerifyComponent implements OnInit {
   licenseBase64: LicenseBase64;
   uploadFilePath: string;
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+  fileAttr = 'Choose File';
+  license: License | undefined;
+
   constructor(
     private router: Router,
     private dialog: DialogService,
@@ -25,12 +30,45 @@ export class VerifyComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  verifyLicense() {
+  uploadFileEvt(imgFile: any) {
+    if (imgFile.target.files && imgFile.target.files[0]) {
+      this.fileAttr = '';
+      /*
+      Array.from(imgFile.target.files).forEach((file: File) => {
+        this.fileAttr += file.name + ' - ';
+      });
+      */
+      this.fileAttr += imgFile.target.files[0];
+      console.info('file: ' + this.fileAttr);
 
+      // HTML5 FileReader API
+      var that = this;
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        var data = e.target.result;
+
+        console.info('data: ' + data);
+        that.licenseBase64.base64 = data;
+      };
+      reader.readAsDataURL(imgFile.target.files[0]);
+
+      // Reset if duplicate image uploaded again
+      this.fileInput!.nativeElement.value = "";
+    } else {
+      this.fileAttr = 'Choose File';
+    }
+  }
+
+  verifyLicense() {
+    this.licenseService.verify(this.licenseBase64!).subscribe((license: License) => {
+      console.info('verifyLicense: ' + license.signature);
+      this.license = license;
+    }, (error) => {
+      console.info('genLicense error: ' + error.status);
+    });
   }
 
   uploadFile() {
-
 
   }
 
