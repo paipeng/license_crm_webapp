f2513371b8a00466ccd2c8fe594889f68bbf8151
Pai Peng <p.peng@s2icode.com>
2021-10-06 08:34:56 +0800

    update login view

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
+import { Version } from '../models/version.model';
 import { CommonService } from '../service/common.service';
 import { DialogService } from '../service/dialog.service';
 import { StorageService } from '../service/storage.service';
 import { UserService } from '../service/user.service';
+import { VersionService } from '../service/version.service';
 
 @Component({
   selector: 'app-login',
@@ -18,13 +20,16 @@ export class LoginComponent implements OnInit {
   saveUsername: boolean;
   passwordImage: string;
   codeImage: string;
+  version: Version = new Version();
+
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
     private common: CommonService,
     private dialog: DialogService,
     private router: Router,
-    private storage: StorageService
+    private storage: StorageService,
+    private versionService: VersionService
   ) {
     this.formModel = formBuilder.group({
       email: ['', [Validators.required]],
@@ -38,6 +43,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getVersion();
   }
 
   goWebsite() {
@@ -100,5 +106,17 @@ export class LoginComponent implements OnInit {
     } else {
       // this.cookie.delete('lotteryUserName');
     }
+  }
+
+
+  getVersion() {
+    const that = this;
+    this.versionService.getVersion().subscribe((version: Version) => {
+      console.log(version);
+      that.storage.setItem('version', version);
+      that.version = version;
+    }, () => {
+      that.storage.removeItem('version');
+    });
   }
 }
